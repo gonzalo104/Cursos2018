1acd90df52354d97b8aef3133e019bcd49dcaec9
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-06-05 19:16:31 -0500

    c

--- CursoNodeJS/06-webserver/server.js
+++ CursoNodeJS/06-webserver/server.js
@@ -1,19 +1,30 @@
 const express = require('express')
 const app = express()
 const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+//express HBS emgine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
+
  
 app.get('/',  (req, res) => {
   //res.send('Hola Mundo');
     res.render('home.hbs',{
-      nombre: "Gonzalo",
-      anio: new Date().getFullYear()
+      nombre: "gonzalo",    
     });
 
 });
+
+app.get('/about',  (req, res) => {  
+    res.render('about.hbs');
+
+});
+
+
  
-app.listen(3000, () => console.log("Escuchando peticiones en el puerto 3000")); 
+app.listen(port, () => console.log(`Escuchando peticiones en el puerto ${port}`)); 
