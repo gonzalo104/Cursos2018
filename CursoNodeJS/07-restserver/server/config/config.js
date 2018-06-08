a76a92916008432f9bb39fa8d2445990a1b8279f
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-06-07 19:25:29 -0500

    ..

--- CursoNodeJS/07-restserver/server/config/config.js
+++ CursoNodeJS/07-restserver/server/config/config.js
@@ -2,3 +2,20 @@
 //PUERTO
 //===================
 process.env.PORT = process.env.PORT || 3000;
+//======================
+//Entorno
+//===================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+//======================
+//Base de datos
+//===================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
+    urlDB = 'mongodb://cafe-user:gonzalozame04@ds047478.mlab.com:47478/cafe';    
+}
+
+process.env.URLDB = urlDB;
+
+
