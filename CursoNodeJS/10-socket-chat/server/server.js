ea52548365a135e719a2cebe6b696768b568b952
gonzalo104 <gonzalozavalamendoza@gmail.com>
2018-06-26 09:44:28 -0500

    maly

--- CursoNodeJS/10-socket-chat/server/server.js
+++ CursoNodeJS/10-socket-chat/server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const socketIO = require('socket.io');
-const http = require('http');
+const http     = require('http');
 
 const path = require('path');
 
-const app = express();
-let server = http.createServer(app);
+const app    = express();
+let   server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port       = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
